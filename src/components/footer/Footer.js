dbe5c8c3b5630db03aabe7675b0965aaf2de1625
Vivi <jizga93@gmail.com>
2022-04-27 22:53:34 +0200

    Hayu logo in the footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { HAYU_LOGO_WHITE } from "../../consts/Consts";
 import styles from "./Footer.module.scss";
 
 export default function Footer() {
   return (
     <div className={styles.container}>
-      <div>LOGO</div>
+      <div>
+        <img src={HAYU_LOGO_WHITE} alt="hayu logo" />
+      </div>
       <div>social And Market</div>
 
       <div>navLinks</div>
