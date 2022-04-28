b43a7c1dae51bfaacf406ca117d7b2883b62e89d
Vivi <jizga93@gmail.com>
2022-04-28 10:50:00 +0200

    Global context updated

--- src/components/homeContent/HomeContent.js
+++ src/components/homeContent/HomeContent.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useGlobalContext } from "../../contexts/GobalContext";
 import Carousel from "../carousel/Carousel";
 import styles from "./HomeContent.module.scss";
 
 export default function HomeContent() {
-  const { data } = useGlobalContext();
+  const { data, setData } = useGlobalContext();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let response = await fetch(
+          "https://api.stg.hayu.com/page/home?platform=web"
+        );
+        response = await response.json();
+        setData(response);
+      } catch (e) {
+        console.error(`Error from database -- ${e}`);
+      }
+    };
+
+    getData();
+  }, [setData]);
+
   return (
     <div className={styles.container}>
       {data?.modules?.length
--- src/contexts/GobalContext.js
+++ src/contexts/GobalContext.js
@@ -1,26 +1,10 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 
 const Context = createContext();
 
 const GlobalContextProvider = ({ children }) => {
   // To add variables inside to the context
   const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        let response = await fetch(
-          "https://api.stg.hayu.com/page/home?platform=web"
-        );
-        response = await response.json();
-        setData(response);
-      } catch (e) {
-        console.error(`Error from database -- ${e}`);
-      }
-    };
-
-    getData();
-  }, []);
 
   return (
     <Context.Provider
